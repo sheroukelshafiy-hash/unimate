38a1e55d1a3558831c744a2dcec3e6f70bc5ea82
menoo1110 <hanydabwhndyh@gmail.com>
2026-09-21 15:34:04 +0300

    Fix login

--- src/app/components/auth/login/login.ts
+++ src/app/components/auth/login/login.ts
@@ -1,37 +1,41 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import {RouterLink} from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+
 @Component({
-  imports: [ReactiveFormsModule, RouterLink], 
+  imports: [ReactiveFormsModule, RouterLink],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
 export class Login {
+
   constructor(private authService: AuthService) {}
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)])
   });
-  login(email: string, password: string):boolean {
-    const user = JSON.parse(localStorage.getItem('user') || 'null');
-    const admin = JSON.parse(localStorage.getItem('admin') || 'null');
-    if (user && user.email === email && user.password === password) {
-      localStorage.setItem('isLoggedIn', 'true');
-      localStorage.setItem('role', user.role);
-      alert('Login successful!');
-      return true;
-    } else if (admin && admin.email === email && admin.password === password) {
-      localStorage.setItem('isLoggedIn', 'true');
-      localStorage.setItem('role', admin.role);
-      alert('Admin login successful!');
-      return true;
-    }       
-    else {
-      alert('Invalid email or password');
-      return false;
+
+  login() {
+
+    if (this.loginForm.invalid) {
+      alert('Please enter valid email and password');
+      return;
+    }
+
+    const email = this.loginForm.value.email!;
+    const password = this.loginForm.value.password!;
+
+    const success = this.authService.login(email, password);
+
+    if (success) {
+      if (this.authService.getRole() === 'admin') {
+        window.location.href = '/admin';
+      } else {
+        window.location.href = '/home';
+      }
     }
   }
-} 
+}
